5db022061597165a0459b081bdc75d78be618d16
jontxu <jon.jc22@gmail.com>
2013-03-06 18:48:08 +0100

    Little changes in class

--- dns/EchoTcpClient.cc
+++ dns/EchoTcpClient.cc
@@ -3,7 +3,15 @@
 
 #include "TcpListener.h"
 
-int main() {
+void usage() {
+  cout << "Usage: EchoTcpClient <name-server-port> <echo-server-domain-name-to-resolve>" << endl;
+  exit(1);
+}
+
+//MODIFICATIONS 2.3.3 & 2.3.5
+//int main() {
+  int main(int argc, char** argv) {
+      if (argc != 3) usage();
 /*
 	// MODIFICATION 2.3.3
 		PracticaCaso::TcpClient * client = new PracticaCaso::TcpClient();
@@ -17,6 +25,7 @@ int main() {
 		delete client;
 */
 	// MODIFICATION 2.3.5
+	/*
 	for (int i=0; i<100; i++) {
 		PracticaCaso::TcpClient * client = new PracticaCaso::TcpClient();
 		cout << "Petición: " << i << endl;
@@ -28,6 +37,33 @@ int main() {
 		cout << "Message received: " << msg << endl;
 		client->close();	
 		delete client;
-	}
+	}*/
+	
+	//modification 2.3.7
+	
+  // CONEXIÓN AL SERVIDOR DE NOMBRES PARA PEDIR DETALLES DEL SERVIDOR DE ECO
+  PracticaCaso::TcpClient cliente;
+  cliente.connect("127.0.0.1", atoi(argv[1]));
+  string dnsName = argv[2];
+  cliente.send(dnsName);
+  string ipAddressAndPort = cliente.receive();
+  cliente.close();
+
+  if (ipAddressAndPort.find("ERROR") == 0)
+      cout << "The DNS name " << dnsName << " could not be resolved." << endl;
+  else {
+      // CONEXIÓN AL SERVIDOR DE ECO OBTENIDO DEL SERVIDOR DE NOMBRES
+      PracticaCaso::TcpClient * client = new PracticaCaso::TcpClient();
+      string ip = ipAddressAndPort.substr(0, ipAddressAndPort.find(":"));
+      int port = atoi((ipAddressAndPort.substr(ipAddressAndPort.find(":")+1)).c_str());
+      client->connect( ip, port );
+      string msg = "¡Hello CASO students!";
+      client->send(msg);
+      cout << "Message sent: " << msg << endl;
+      msg = client->receive();
+      cout << "Message received: " << msg << endl;
+      client->close();	
+      delete client;
+  }
 }
 
---
+++ dns/path.sh
@@ -0,0 +1,2 @@
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/jon/Code/NameServer/util
+export LD_LIBRARY_PATH
